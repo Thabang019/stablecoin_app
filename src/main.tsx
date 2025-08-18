603e9cf71080679966090c24ab9402b1a74f0584
Thabang019 <128829624+Thabang019@users.noreply.github.com>
2025-08-18 11:57:25 +0200

    Updated package-lock.json

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import ErrorBoundary from './components/ErrorBoundary'
 import LandingPage from './pages/Welcome'
 import LoginPage from './pages/SignIn'
 import SignUpPage from './pages/SignUp'
@@ -14,31 +13,20 @@ import RequestDetailsPage from './pages/RequestDetails'
 import CollaborativeDashboard from './pages/PaymentsDashboard'
 
 import { registerSW } from 'virtual:pwa-register'
-import { logger } from './utils/logger'
-
-// Initialize logger
-logger.info('Application starting', { 
-  version: import.meta.env.VITE_APP_VERSION || '1.0.0',
-  environment: import.meta.env.MODE 
-});
 
 registerSW({ immediate: true })
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <ErrorBoundary>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignUpPage />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/send" element={<SendMoneyPage />} />
-          <Route path="/request/:requestId" element={<RequestDetailsPage />} />
-          <Route path="/collaborative" element={<CollaborativeDashboard />} />
-        </Routes>
-      </BrowserRouter>
-    </ErrorBoundary>
-  </StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignUpPage />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/profile" element={<ProfilePage />} />
+      <Route path="/send" element={<SendMoneyPage />} />
+      <Route path="/request/:requestId" element={<RequestDetailsPage />} />
+      <Route path="/collaborative" element={<CollaborativeDashboard />} />
+    </Routes>
+  </BrowserRouter>
 )
